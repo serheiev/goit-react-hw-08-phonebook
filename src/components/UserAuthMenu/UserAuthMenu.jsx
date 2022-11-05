910e3dca8709f644cd4ff0ff812deb1dfeb0ce1d
serheiev <69833095+serheiev@users.noreply.github.com>
2022-11-05 22:24:23 +0200

    fix warn

--- src/components/UserAuthMenu/UserAuthMenu.jsx
+++ src/components/UserAuthMenu/UserAuthMenu.jsx
@@ -1,19 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from 'redux/auth/auth-operations';
 import { selectName } from 'redux/auth/auth-selectors';
-import s from './UserAuthMenu.module.css';
 
 export const UserAuthMenu = () => {
   const dispatch = useDispatch();
   const name = useSelector(selectName);
   return (
-    <div className={s.authWrap}>
-      <p className={s.authPar}>{`Welcome ${name}!`}</p>
-      <button
-        className={s.authBtn}
-        variant="outlined"
-        onClick={() => dispatch(logout())}
-      >
+    <div>
+      <p>{`Welcome ${name}!`}</p>
+      <button variant="outlined" onClick={() => dispatch(logout())}>
         Log Out
       </button>
     </div>
